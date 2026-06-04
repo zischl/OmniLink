f7e715966a71d0d5e45bf54a1f10a2f40d5176e4
zischl <supermechs456@gmail.com>
2026-06-04 07:10:56 +0530

    - Capture System Cross Platform Update
    - Implemented a variant approach for storing multiple EncodeStream
    formats in a generic unordered_map.
    - Removed platform based CaptureController.* files and migrated to a
    cross compatible version
    - Concept enforced encode stream capture source.

---
+++ src/capture/CaptureController.cpp
@@ -0,0 +1,87 @@
+#include "CaptureController.h"
+#include "Capture.h"
+#include "nvenc.h"
+#include <utility>
+
+#if defined(_WIN32)
+CaptureController::StreamID CaptureController::AddStream(ID3D11Device* D3D11Device,
+                                                         ID3D11DeviceContext* D3D11Context,
+                                                         session* NetSession,
+                                                         DeviceMap TargetID,
+                                                         CaptureMode Mode)
+{
+    StreamID id = StreamCount++;
+
+    if (Mode == CaptureMode::WGC) {
+        ScreenCaptureWGC* WGSCapture = new ScreenCaptureWGC(D3D11Device, D3D11Context);
+        ID3D11Texture2D* CaptureBuffer = nullptr;
+        WGSCapture->CreateWGCBuffer(D3D11Device, &CaptureBuffer);
+        WGSCapture->CreateMonitorCapSession(CaptureBuffer, 1920, 1080);
+        WGSCapture->StartSession();
+
+        NvencSession* Encoder =
+            new NvencSession(D3D11Device, NvEncodeAPI.NVFunctions, CaptureBuffer, 1920, 1080);
+
+        Streams.try_emplace(id, std::in_place_type<EncodeStream<ScreenCaptureWGC>>);
+        std::get<EncodeStream<ScreenCaptureWGC>>(Streams[id])
+            .Start(WGSCapture, Encoder, NetSession, TargetID);
+
+    } else if (Mode == CaptureMode::DXGI) {
+        ScreenCaptureDXGI* DXGISCapture = new ScreenCaptureDXGI();
+        DXGISCapture->InitDXGI(D3D11Device);
+        ID3D11Texture2D* CaptureBuffer = DXGISCapture->GetBuffer();
+
+        NvencSession* Encoder =
+            new NvencSession(D3D11Device, NvEncodeAPI.NVFunctions, CaptureBuffer, 1920, 1080);
+
+        Streams.try_emplace(id, std::in_place_type<EncodeStream<ScreenCaptureDXGI>>);
+        std::get<EncodeStream<ScreenCaptureDXGI>>(Streams[id])
+            .Start(DXGISCapture, Encoder, NetSession, TargetID);
+    }
+
+    return id;
+}
+#elif defined(__linux__)
+StreamID CaptureController::AddStream(session* NetSession, DeviceMap TargetID, CaptureMode Mode)
+{
+    StreamID id = StreamCount++;
+
+    return id;
+}
+#endif
+
+void CaptureController::RemoveStream(size_t StreamID)
+{
+    auto iter = Streams.find(StreamID);
+
+    if (iter != Streams.end()) {
+        std::visit(
+            [](auto& stream) {
+                stream.Stop();
+                // might have to handle cleanup later properly
+                if (stream.Source)
+                    delete stream.Source;
+                if (stream.Encoder)
+                    delete stream.Encoder;
+            },
+            iter->second);
+        Streams.erase(iter);
+    }
+}
+
+void CaptureController::StopAll()
+{
+    for (auto& [id, variant_stream] : Streams) {
+        std::visit(
+            [](auto& stream) {
+                stream.Stop();
+                if (stream.Source)
+                    delete stream.Source;
+                if (stream.Encoder)
+                    delete stream.Encoder;
+            },
+            variant_stream);
+    }
+    Streams.clear();
+    StreamCount = 0;
+}
---
+++ src/capture/CaptureController.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "Capture.h"
+#include "EncodeStream.h"
+#include "OmniEnums.h"
+#include "nvenc.h"
+#include <unordered_map>
+#include <variant>
+
+#if defined(_WIN32)
+
+enum CaptureMode { DXGI, WGC };
+
+using EncodeStreamTypes =
+    std::variant<EncodeStream<ScreenCaptureDXGI>, EncodeStream<ScreenCaptureWGC>>;
+#elif defined(__linux__)
+
+enum CaptureMode { PW, X11_SHM };
+
+using EncodeStreamTypes = std::variant<EncodeStream<ScreenCapturePW>>;
+#endif
+
+struct RendererCoreState
+{
+#if defined(_WIN32)
+    ID3D11Device* Device;
+    ID3D11DeviceContext* Context;
+#elif defined(__linux__)
+    VkDevice Device;
+    VkQueue Queue;
+#endif
+};
+
+struct CaptureController
+{
+    using StreamID = size_t;
+
+  private:
+    std::unordered_map<size_t, EncodeStreamTypes> Streams;
+    size_t StreamCount = 0;
+
+  public:
+    NVENCODER NvEncodeAPI;
+
+#if defined(_WIN32)
+    StreamID AddStream(ID3D11Device* D3D11Device,
+                       ID3D11DeviceContext* D3D11Context,
+                       session* NetSession,
+                       DeviceMap TargetID,
+                       CaptureMode Mode);
+#elif defined(__linux__)
+    StreamID AddStream(session* NetSession, DeviceMap TargetID, CaptureMode Mode);
+#endif
+
+    void RemoveStream(size_t StreamID);
+
+    void StopAll();
+};
